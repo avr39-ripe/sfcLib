8a4bef261a0552dea87ff91642002b35fd2a0b9d
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-04-03 23:49:29 +0300

    Implement FanClass to control fan. Needs polishing.

---
+++ fan.cpp
@@ -0,0 +1,110 @@
+/*
+ * FanClass.cpp
+ *
+ *  Created on: 3 апр. 2016
+ *      Author: shurik
+ */
+
+#include <fan.h>
+
+// FanClass
+
+FanClass::FanClass(TempSensors &tempSensor, ThermostatClass &thermostat, BinInClass &startButton, BinInClass &stopButton, BinOutClass &fanRelay)
+{
+	_tempSensor = &tempSensor;
+	_thermostat = &thermostat;
+	_startButton = &startButton;
+	_stopButton = &stopButton;
+	_fanRelay = &fanRelay;
+	_startButton->onStateChange(onStateChangeDelegate(&FanClass::_modeStart, this));
+	_stopButton->onStateChange(onStateChangeDelegate(&FanClass::_modeStop, this));
+	_fanRelay->setState(false);
+	_thermostat->onStateChange(onStateChangeDelegate(&BinOutClass::setState, _fanRelay));
+	_thermostat->stop();
+};
+
+void FanClass::_modeStart(uint8_t state)
+{
+	if (state)
+	{
+		_mode = FanMode::START;
+		Serial.printf("START Button pressed\n");
+		_fanRelay->setState(true);
+		//TODO: CHANGE THIS LATER FOR 60000!!!
+		_fanTimer.initializeMs(_startDuration * 6000, TimerDelegate(&FanClass::_modeStartEnd, this)).start(false);
+	}
+}
+
+void FanClass::_modeStartEnd()
+{
+	Serial.printf("START Finished\n");
+	_fanRelay->setState(false);
+	_thermostat->start();
+	//TODO: CHANGE THIS LATER FOR 60000!!!
+	_fanTimer.initializeMs(_periodicInterval * 6000, TimerDelegate(&FanClass::_pereodic, this)).start(false);
+	_mode = FanMode::RUN;
+}
+
+void FanClass::_pereodic()
+{
+	Serial.printf("PREIODIC START\n");
+	_periodicStartTemp = _tempSensor->getTemp();
+	_thermostat->stop();
+	_fanRelay->setState(true);
+	//TODO: CHANGE THIS LATER FOR 60000!!!
+	_fanTimer.initializeMs(_periodicDuration * 6000, TimerDelegate(&FanClass::_pereodicEnd, this)).start(false);
+	_mode = FanMode::PERIODIC;
+}
+
+void FanClass::_pereodicEnd()
+{
+	Serial.printf("PREIODIC END\n");
+	float _periodicEndTemp = _tempSensor->getTemp();
+	Serial.printf("Periodic end - startTemp: ");Serial.print(_periodicStartTemp);Serial.printf(" endTemp ");Serial.println(_periodicEndTemp);
+	if (_periodicEndTemp - _periodicStartTemp > (float)(_periodicTempDelta / 100))
+	{
+		_periodicCounter = maxLowTemp;
+	} else if (_periodicCounter > 0)
+	{
+		Serial.printf("Seems like no wood left, giving another chance!\n");
+		_periodicCounter--;
+	}
+	if (!_periodicCounter)
+	{
+		Serial.printf("No wood left! going idle!\n");
+		_fanRelay->setState(false);
+		_thermostat->stop();
+		_fanTimer.stop();
+		_mode = FanMode::IDLE;
+	}
+	else
+	{
+		Serial.printf("PERIODIC - GO TO RUN MODE\n");
+		_fanRelay->setState(false);
+		_thermostat->start();
+		//TODO: CHANGE THIS LATER FOR 60000!!!
+		_fanTimer.initializeMs(_periodicInterval * 6000, TimerDelegate(&FanClass::_pereodic, this)).start(false);
+		_mode = FanMode::RUN;
+	}
+}
+
+void FanClass::_modeStop(uint8_t state)
+{
+	if (state)
+	{
+		_mode = FanMode::STOP;
+		Serial.printf("STOP Button pressed\n");
+		_fanRelay->setState(true);
+		//TODO: CHANGE THIS LATER FOR 60000!!!
+		_fanTimer.initializeMs(_startDuration * 6000, TimerDelegate(&FanClass::_modeStopEnd, this)).start(false);
+	}
+}
+
+void FanClass::_modeStopEnd()
+{
+	Serial.printf("STOP Finished\n");
+	_fanRelay->setState(false);
+	_thermostat->stop();
+	_fanTimer.stop();
+	_mode = FanMode::IDLE;
+}
---
+++ fan.h
@@ -0,0 +1,69 @@
+/*
+ * FanClass.h
+ *
+ *  Created on: 3 апр. 2016
+ *      Author: shurik
+ */
+
+#ifndef LIB_FAN_FAN_H_
+#define LIB_FAN_FAN_H_
+#include <SmingCore/SmingCore.h>
+#include <tempsensors.h>
+#include <binin.h>
+#include <binout.h>
+#include <thermostat.h>
+
+namespace FanMode
+{
+	const uint8_t IDLE=0;
+	const uint8_t START=1;
+	const uint8_t RUN=2;
+	const uint8_t PERIODIC=3;
+	const uint8_t STOP=4;
+}
+
+const uint8_t maxLowTemp = 2;
+
+class FanClass
+{
+public:
+	FanClass(TempSensors &tempSensor, ThermostatClass &thermostat, BinInClass &startButton, BinInClass &stopButton, BinOutClass &fanRelay);
+	virtual ~FanClass() {};
+	void start();
+	void stop();
+	uint8_t getMode() const { return _mode;	}
+	void setMode(uint8_t mode) { _mode = mode; }
+	uint16_t getStartDuration() const { return _startDuration; }
+	void setStartDuration(uint16_t startDuration) { _startDuration = startDuration; }
+	uint16_t getStopDuration() const { return _stopDuration; };
+	void setStopDuration(uint16_t stopDuration) { _stopDuration = stopDuration; };
+	uint16_t getPeriodicInterval() const { return _periodicInterval; };
+	void setPeriodicInterval(uint16_t periodicInterval) { _periodicInterval = periodicInterval; };
+	uint16_t getPeriodicDuration() const { return _periodicDuration; };
+	void setPeriodicDuration(uint16_t periodicDuration) { _periodicDuration = periodicDuration; };
+	uint16_t getPeriodicTempDelta() const { return _periodicTempDelta; };
+	void setPeriodicTempDelta(uint16_t periodicTempDelta) { _periodicTempDelta = periodicTempDelta; };
+private:
+	void _modeStart(uint8_t state);
+	void _modeStartEnd();
+	void _modeStop(uint8_t state);
+	void _modeStopEnd();
+	void _pereodic();
+	void _pereodicEnd();
+	uint16_t _timerInterval = 0;
+	Timer _fanTimer;
+	TempSensors* _tempSensor;
+	BinInClass* _startButton;
+	BinInClass* _stopButton;
+	BinOutClass* _fanRelay;
+	ThermostatClass* _thermostat;
+	uint8_t _mode = FanMode::STOP;
+	uint16_t _startDuration = 1; // turn Fan on for this duration when START button pressed, Minutes
+	uint16_t _stopDuration = 1; // turn Fan on for this duration when STOP button pressed, Minutes
+	uint16_t _periodicInterval = 5; // interval to turn on Fan in RUN mode, Minutes
+	uint16_t _periodicDuration = 1; // duration to turn on Fan in RUN mode, Minutes
+	float _periodicStartTemp = 0; // Temperature at periodic start, will be compared to temperature at periodic and
+	uint16_t _periodicTempDelta = 500; // minimum temperature difference after periodic fan turn on, if less go to IDLE mode
+	uint8_t _periodicCounter = maxLowTemp; // if 0 o to IDLE mode;
+};
+#endif /* LIB_FAN_FAN_H_ */
