c850457c6922cbfdfb28f1b058827d82ab7781d7
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-07-18 17:50:21 +0300

    Remove unused methods and commented out code

--- binio/binhttpbutton.h
+++ binio/binhttpbutton.h
@@ -15,7 +15,6 @@ class BinHttpButtonClass : public BinInClass
 {
 public:
 	BinHttpButtonClass(HttpServer& webServer, uint8_t unitNumber, String name = "Button", BinStateClass *outputState = nullptr);
-//	: BinInClass(unitNumber, 1), _webServer(webServer), _name(name), _output(output) {};
 	virtual ~BinHttpButtonClass() {};
 	String getName() { return _name; };
 	void setName(String name) { _name = name; };
@@ -32,15 +31,10 @@ private:
 class BinHttpButtonsClass
 {
 public:
-//	BinHttpButtonsClass::BinHttpButtonsClass(HttpServer& webServer)
-//	: _webServer(webServer) {};
 	void onWSReceiveButton(JsonObject& jsonRoot);
 	void add(BinHttpButtonClass* button);
-	void setButton(uint8_t button, uint8_t state) { _buttons[button]->state.set(state); };
-	uint8_t getButton(uint8_t button) { return _buttons[button]->getOutputState(); };
 	void onWSGetButtons(WebSocket& socket);
 private:
 	Vector<BinHttpButtonClass*> _buttons;
-//	HttpServer& _webServer;
 };
 #endif /* LIB_BINIO_BINHTTPBUTTON_H_ */
